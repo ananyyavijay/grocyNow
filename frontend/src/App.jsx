50ba0927dd451ed2f297a7393b6c59f2f46d6918
ananyyavijay <ananyavijay05@gmail.com>
2025-11-25 00:20:34 +0530

    created login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+import MyOrders from './pages/MyOrders'
+import { useAppContext } from './context/AppContext'
+import Auth from './modals/Auth'
 function App() {
+  const { showUserLogin, isSeller } = useAppContext();
+  const isSellerPath = useLocation().pathname.includes("seller");
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div>
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Auth /> : null}
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          {/* <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/product/:category/:id" element={<SingleProduct />} /> */}
+          <Route path="/cart" element={<Cart />} />
+          {/* <Route path="/add-address" element={<Address />} /> */}
+          <Route path="/my-orders" element={<MyOrders />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
