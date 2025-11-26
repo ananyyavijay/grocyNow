e66ad66716efe0c1821d951b4ea4c1ebfe0811d2
ananyyavijay <ananyavijay05@gmail.com>
2025-11-27 00:06:45 +0530

    created cart/add-address routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
@@ -12,6 +11,7 @@ import Auth from './modals/Auth'
 import Footer from './components/Footer.jsx'
 import { Toaster } from 'react-hot-toast'
 import SingleProduct from './pages/SingleProduct.jsx'
+import AddAddress from './pages/AddAddress.jsx'
 
 function App() {
   const { showUserLogin, isSeller } = useAppContext();
@@ -29,7 +29,7 @@ function App() {
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/product/:category/:id" element={<SingleProduct />} />
           <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/add-address" element={<Address />} /> */}
+          <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
         </Routes>
       </div>
