6d4d9fe9eeb98caf75fd7f69f1398b7d6ddfd86e
ananyyavijay <ananyavijay05@gmail.com>
2025-12-01 21:11:19 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+
+const app = express();
+
+const allowedOrigins = [
+    "http://localhost:5173"
+];
+
+//middlewares
+app.use(express.json());
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+app.use(cookieParser());
+
+//api endpoints
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
