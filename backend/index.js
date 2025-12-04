aa2f08c294522f6228528e727a4f6271b43c7989
ananyyavijay <ananyavijay05@gmail.com>
2025-12-04 23:33:18 +0530

    created cart controller and routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 import userRoutes from './routes/user.routes.js';
 import sellerRoutes from './routes/seller.routes.js'
 import productRoutes from './routes/product.routes.js'
+import cartRoutes from './routes/cart.routes.js'
 import { connectCloudinary } from './config/cloudinary.js';
 
 const app = express();
@@ -34,6 +35,7 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/seller", sellerRoutes);
 app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
