485b7b1aa9b8288336844ff2fc4d8afb9084a8d5
ananyyavijay <ananyavijay05@gmail.com>
2025-12-06 02:10:23 +0530

    fixed bug in home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero.jsx";
 import Category from "../components/Category.jsx";
 import BestSeller from "../components/BestSeller.jsx";
-import NewsLetter from "../components/NewsLetter.jsx";
+import NewsLetter from "../components/Newsletter.jsx";
 
 const Home = () => {
   return (
